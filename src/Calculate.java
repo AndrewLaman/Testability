6c0b1295090931c613c7fb8edcb85bc3b336db4f
Andrew Laman <andrewlaman17061992@gmail.com>
2024-10-08 14:14:09 +0400

    git@github.com:AndrewLaman/Testability.git

---
+++ src/Calculate.java
@@ -0,0 +1,7 @@
+public class Calculate {
+
+    public int calculate(int price) {
+        int miles = price / 20; // Делим стоимость билета на 20 для получения миль
+        return miles;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import BonusMilesServise.BonusMilesService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.getClass().getModifiers();
+        System.out.println(miles);
+    }
+    BonusMilesService service = new BonusMilesService();
+}
